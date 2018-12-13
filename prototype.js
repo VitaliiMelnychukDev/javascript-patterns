de9b1bb87f28699bd4d573ac067aff5cf6587eb7
VitaliiMelnychukDev <vitaliymelnychukdev@gmail.com>
2018-12-13 22:48:01 +0200

    Prototype pattern

---
+++ prototype.js
@@ -0,0 +1,11 @@
+const Car = function (make, model) {
+    this.make = make;
+    this.model = model;
+};
+
+Car.prototype.printInfo = function () {
+    console.log('Car make: ' + this.make + '; Car model: ' + this.model + '.');
+};
+
+const audiA8 = new Car('Audi', 'A8');
+audiA8.printInfo();
